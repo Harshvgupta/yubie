e35777a12c996e89bd4a6291809faae8cd447a44
Pranomvignesh <pranomvignesh@gmail.com>
2023-10-05 19:10:11 -0700

    Folder Structure

--- yubie/main.py
+++ yubie/main.py
@@ -1,3 +1,36 @@
+from enum import Enum
+from dataclasses import dataclass
+
+
+class DetectableClasses(Enum):
+    BALL = 1
+    PERSON = 2
+    
+class ImageLabels(Enum):
+    FRONTLEFT_FISHEYE_IMAGE = 1
+    FRONTRIGHT_FISHEYE_IMAGE = 2
+    LEFT_FISHEYE_IMAGE = 3
+    RIGHT_FISHEYE_IMAGE = 4
+    UNITREE_IMAGE = 5
+
+class ImageSources(Enum):
+    TRAINING = 1
+    SPOT_LIVE = 2
+    UNITREE_LIVE = 3
+@dataclass
+class Image:
+    data: ImageBuffer
+    label: ImageLabels
+    src: ImageSources
+    
+@dataclass
+class BoundingBox:
+    x: float
+    y: float
+    w: float
+    h: float
+    
+
 def init():
     pass
 
